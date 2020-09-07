93c74ab5c6ebc70b41dd245832a49cb15eedcc90
camiloibanez <camiloibanez8@gmail.com>
2020-09-07 11:36:58 -0500

    slideshows in sync

--- static/js/logic.js
+++ static/js/logic.js
@@ -14,16 +14,29 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+  var captionText = document.getElementById("caption1");
+
+  var slides2 = document.getElementsByClassName("mySlides2");
+  var dots2 = document.getElementsByClassName("demo2");
+  var captionText2 = document.getElementById("caption2");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    slides2[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
+    dots2[i].className = dots2[i].className.replace(" active", "");
   }
+
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
+
+  slides2[slideIndex-1].style.display = "block";
+  dots2[slideIndex-1].className += " active";
+  captionText2.innerHTML = dots2[slideIndex-1].alt;
 }
