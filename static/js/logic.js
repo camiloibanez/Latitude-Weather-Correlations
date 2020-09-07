8154af47b069acbde5954c9c06c8067d4d61fa3a
camiloibanez <camiloibanez8@gmail.com>
2020-09-07 12:00:59 -0500

    added description

--- static/js/logic.js
+++ static/js/logic.js
@@ -21,6 +21,8 @@ function showSlides(n) {
   var dots2 = document.getElementsByClassName("demo2");
   var captionText2 = document.getElementById("caption2");
 
+  var description = document.getElementById("description");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
 
@@ -40,4 +42,19 @@ function showSlides(n) {
   slides2[slideIndex-1].style.display = "block";
   dots2[slideIndex-1].className += " active";
   captionText2.innerHTML = dots2[slideIndex-1].alt;
+
+  if (n == 1) {
+    description.innerHTML = "Here the maximum temperature of cities plotted against their latitude. The trend shows that max temperatures decrease the farther from the equator the city is.";
+  }
+
+  else if (n == 2) {
+    description.innerHTML = "Here the humidity of cities is plotted against their latitude. There doesn't seem to be a strong trend between humidity and latitude.";
+  }
+  else if (n == 3) {
+    description.innerHTML = "Here the cloudiness of cities plotted against their latitude. There seems to be horizontal lines along the easily divisible numbers weathermen like to forecast data on.";
+  }
+  else {
+    description.innerHTML = "Here the wind speed of cities is plotted against their latitude. It appears that higher wind speeds occur farther from the equator.";
+  }
+
 }
